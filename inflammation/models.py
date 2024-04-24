281c94e579fe18502918c8785f958cef28586714
jakobchristiaanse <jakob.christiaanse@live.de>
2024-04-24 11:31:28 +0200

    Added Person, Patient, and Doctor classes, and tests for them. Included tests in github workflow

--- inflammation/models.py
+++ inflammation/models.py
@@ -11,6 +11,76 @@ from functools import reduce
 import numpy as np
 
 
+class Observation:
+    """
+    Class representing a patient's observations.
+
+    Attributes:
+        day
+        value
+    """
+
+    def __init__(self, day, value):
+        self.day = day
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Person:
+    """
+    Class representing a person.
+    """
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+
+class Patient(Person):
+    """A patient in an inflammation study."""
+
+    def __init__(self, name):
+        super().__init__(name)
+        self.observations = []
+
+    def add_observation(self, value, day=None):
+        if day is None:
+            try:
+                day = self.observations[-1].day + 1
+
+            except IndexError:
+                day = 0
+
+        new_observation = Observation(day, value)
+
+        self.observations.append(new_observation)
+        return new_observation
+
+
+class Doctor(Person):
+    """A doctor in an inflammation study."""
+
+    def __init__(self, name):
+        super().__init__(name)
+        self.patients = []
+
+    def add_patient(self, patient):
+
+        if isinstance(patient, Patient):
+            new_patient = patient
+        else:
+            new_patient = Patient(patient)
+
+        if new_patient not in self.patients:
+            self.patients.append(new_patient)
+            return new_patient
+        else:
+            print('Patient with name %s already in Doctor %s\'s list.' % (patient, self.name))
+
 def load_csv(filename):
     """Load a Numpy array from a CSV
 
